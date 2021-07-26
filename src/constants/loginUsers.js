1cd68a2c77a7a56371acabd97f9f845be6d2cc75
maniraj0601 <iammaniraj06@gmail.com>
2021-07-26 12:37:43 +0530

    homepage removed from package,json

--- src/constants/loginUsers.js
+++ src/constants/loginUsers.js
@@ -1,14 +1,14 @@
 export const LOGINUSER = {
   users: [
     {
-      sessionToken: '12345',
+      sessionToken: '12345abc',
       username: "hruday@gmail.com",
       password: "hruday123",
     },
     {
       sessionToken: '12345asd',
       username: "iammaniraj06@gmail.com",
-      password: "qwe",
+      password: "test",
     }
   ],
 };
